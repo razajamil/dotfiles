3d5b9328abb901dbadb3e1fedd8613203e0c5615
Raza Jamil <raza.jamil@reckon.com>
2026-04-19 20:15:24 +1000

    kitty and nvim updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,15 @@ return {
   {
     "oskarnurm/koda.nvim",
     priority = 1000,
+    opts = function()
+      local glade = require("koda.palette.glade")
+
+      return {
+        colors = {
+          line = "#bebbbb",
+        },
+      }
+    end,
   },
   {
     "LazyVim/LazyVim",
